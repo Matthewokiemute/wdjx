25176d54478822d4395588a2cf721d7c6ddef2d0
Matthewokiemute <ego.okiemute@gmail.com>
2023-09-22 17:27:03 +0100

    Added animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { TbArrowUpRight } from "react-icons/tb";
+import "aos/dist/aos.css";
 
 const HeroSection = () => {
   return (
     <div className="mx-3 my-10 lg:my-20 lg:px-40 ">
-      <h1 className="text-5xl md:text-6xl lg:text-8xl font-medium mb-5 lg:mb-10 text-white">
+      <h1 className="text-5xl md:text-6xl lg:text-8xl font-medium mb-5 lg:mb-10 text-white" data-aos="fade-up" data-aos-delay="400">
         The First Waffi <br className="hidden lg:inline-block" />
         Design Conference
       </h1>
-      <p className="mb-5 lg:mb-10 md:text-[17px] text-[#ddd] lg:w-3/4">
+      <p className="mb-5 lg:mb-10 md:text-[17px] text-[#ddd] lg:w-3/4" data-aos="fade-up" data-aos-delay="600">
         Combining modern designs with the Africans vibes to lite our Waffi
         people and city. As Warri nor dey carry last, if you sabi am and say you
         nor wan make you and your career take last secure your spot now.
       </p>
-      <button class="flex items-center space-x-1 bg-[#FFC800] text-black text-[18px] px-6 py-4 hover:opacity-80 ease-in duration-200">
+      <button class="flex items-center space-x-1 bg-[#FFC800] text-black text-[18px] px-6 py-4 hover:opacity-80 ease-in duration-200" data-aos="fade-left" data-aos-delay="800">
         <a href="https://wa.me/message/PHODOTZ5RGMWC1">Make I secure my sit first</a>
         <TbArrowUpRight className="w-6 h-6" />
       </button>
